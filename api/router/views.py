62e80dce5503006be0bbcf065757bd7243ab4772
Junyeong Yim <xjuny1996@gmail.com>
2021-07-01 11:48:24 +0900

    ID/IP, 닉네임 검색 추가
    Detail
    ======
     - 역순으로 출력

--- api/router/views.py
+++ api/router/views.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
 
 from api.models.post.models import Post
@@ -5,7 +7,34 @@ from api.models.post.models import Post
 from .serializers import PostSerializer
 
 
-# Create your views here.
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().order_by('-num')
     serializer_class = PostSerializer
+    http_method_names = ['get', 'post']
+
+    def filter_queryset(self, queryset):
+        qs = queryset
+        name = self.request.GET.get('name')
+        idip = self.request.GET.get('idip')
+        if name:
+            qs = qs.filter(name__contains=name)
+        if idip:
+            qs = qs.filter(idip__contains=idip)
+        return qs
+
+    @swagger_auto_schema(manual_parameters=[
+        openapi.Parameter('name',
+                          openapi.IN_QUERY,
+                          description='닉네임',
+                          type=openapi.TYPE_STRING),
+        openapi.Parameter('idip',
+                          openapi.IN_QUERY,
+                          description='아이디/아이피',
+                          type=openapi.TYPE_STRING)
+    ])
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(auto_schema=None)
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
