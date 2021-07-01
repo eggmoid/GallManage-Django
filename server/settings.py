8340e5d0d5747ab8e4eb664f89949def90b8f7f4
Junyeong Yim <xjuny1996@gmail.com>
2021-07-02 03:14:57 +0900

    ALLOWED_HOSTS에 아이피 추가
    Detail
    ======
     - secrets.json에도 추가

--- server/settings.py
+++ server/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = get_secret('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost'] + get_secret('ALLOWED_HOSTS')
 
 # Application definition
 
