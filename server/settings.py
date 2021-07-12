36c745a75610db3e32b09597529b9b473e869fa4
Junyeong Yim <xjuny1996@gmail.com>
2021-07-12 23:37:24 +0900

    DB 로그 파일로 출력
    Detail
    ======
     - docker-compose exec django tail -f /usr/src/app/logfile.log

--- server/settings.py
+++ server/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import json
 import os
+from os.path import join
 import redis
 
 from celery.schedules import crontab
@@ -73,7 +74,21 @@ REST_FRAMEWORK = {
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'fileFormat': {
+            'format':
+                '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s',
+            'datefmt':
+                '%d/%b/%Y %H:%M:%S'
+        }
+    },
     'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': join(BASE_DIR, 'logfile.log'),
+            'formatter': 'fileFormat'
+        },
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
@@ -81,7 +96,8 @@ LOGGING = {
     },
     'loggers': {
         'django.db.backends': {
-            'handlers': ['console'],
+            # 'handlers': ['console'],
+            'handlers': ['file'],
             'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
             'propagate': False,
         }
