042928cec1cec57a5f8cf24829f7a282498e996f
Junyeong Yim <xjuny1996@gmail.com>
2021-07-16 09:47:26 +0900

    파비콘 추가
    Detail
    ======
     - 클로버아이콘 (직접그림ㅎㅎ)

--- server/settings.py
+++ server/settings.py
@@ -131,7 +131,7 @@ SWAGGER_SETTINGS = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
