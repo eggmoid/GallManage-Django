23b13b081a937380a3d15ee635c1e029c2b11b9a
Junyeong Yim <xjuny1996@gmail.com>
2021-08-02 20:26:07 +0900

    Meta클래스에서 month 어트리뷰트 제거
    Detail
    ======
     - invalid attribute가 있다면서 오류남
     - month 들어갈 부분에 중복해서 넣어주는 식으로 변경

--- api/models/post_rank/models.py
+++ api/models/post_rank/models.py
@@ -8,5 +8,5 @@ class PostRank(Post):
     class Meta:
         month = datetime.datetime.today().month
         proxy = True
-        verbose_name = f'{month}월 갤창랭킹'
-        verbose_name_plural = f'{month}월 갤창랭킹'
+        verbose_name = f'{datetime.datetime.today().month}월 갤창랭킹'
+        verbose_name_plural = f'{datetime.datetime.today().month}월 갤창랭킹'
