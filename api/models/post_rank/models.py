8745815685ca35eb3e68837de8990cf5afe22c6c
Junyeong Yim <xjuny1996@gmail.com>
2021-08-02 20:29:15 +0900

    Meta클래스에서 month 어트리뷰트 제거
    Detail
    ======
     - 빼는거깜빡함

--- api/models/post_rank/models.py
+++ api/models/post_rank/models.py
@@ -6,7 +6,6 @@ from api.models.post.models import Post
 class PostRank(Post):
 
     class Meta:
-        month = datetime.datetime.today().month
         proxy = True
         verbose_name = f'{datetime.datetime.today().month}월 갤창랭킹'
         verbose_name_plural = f'{datetime.datetime.today().month}월 갤창랭킹'
