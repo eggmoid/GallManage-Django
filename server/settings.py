f279802135d96c0c723c52fdeb265c199c77be2c
Junyeong Yim <xjuny1996@gmail.com>
2021-08-21 15:18:44 +0900

    서버 에러 로깅
    Detail
    ======
     - 500에러 메시지가 안보임,,
     - touch server.log

--- server/settings.py
+++ server/settings.py
@@ -78,12 +78,24 @@ REST_FRAMEWORK = {
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
     'formatters': {
         'fileFormat': {
             'format':
                 '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s',
             'datefmt':
                 '%d/%b/%Y %H:%M:%S'
+        },
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[%(server_time)s] %(message)s'
         }
     },
     'handlers': {
@@ -96,6 +108,12 @@ LOGGING = {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
+        },
+        'django.server': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': join(BASE_DIR, 'server.log'),
+            'formatter': 'django.server',
         }
     },
     'loggers': {
@@ -103,6 +121,11 @@ LOGGING = {
             # 'handlers': ['console'],
             'handlers': ['file'],
             'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+        'django.server': {
+            'handlers': ['django.server'],
+            'level': 'INFO',
             'propagate': False,
         }
     }
