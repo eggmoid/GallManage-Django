77e6d9ea37894ccc78e93ffedb73e4ec81676d00
Junyeong Yim <xjuny1996@gmail.com>
2021-08-25 14:26:27 +0900

    POST_BACK 문구 수정
    Detail
    ======
     - admin 제외한 유저한테 아예 안보이게 빼는 방법 알아보는중

--- api/models/post_back/models.py
+++ api/models/post_back/models.py
@@ -13,5 +13,5 @@ class BPost(models.Model):
 
     class Meta:
         db_table = 'bpost'
-        verbose_name = '게시글 (백업)'
-        verbose_name_plural = '글 목록 (백업)'
+        verbose_name = '게시글 (백업) - 클릭자제 (매우 느림)'
+        verbose_name_plural = '글 목록 (백업) - 클릭자제 (매우 느림)'
