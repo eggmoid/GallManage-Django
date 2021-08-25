ca6630de01c0100c69249380fde82c96fed5ebb6
Junyeong Yim <xjuny1996@gmail.com>
2021-08-25 13:06:15 +0900

    wsgi.py TNS_ADMIN 주석처리
    Detail
    ======
     - 디버그모드 끄면 작동안함 (이유는 아직 모르겠음)

--- server/wsgi.py
+++ server/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from .settings import get_secret
+# from .settings import get_secret
 
-os.environ['TNS_ADMIN'] = get_secret('TNS_ADMIN')
+# os.environ['TNS_ADMIN'] = get_secret('TNS_ADMIN')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
